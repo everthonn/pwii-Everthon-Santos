747c03bd705b47bbbb528075be33028be2eba9c2
Everthon Luiz <everthondier4@gmail.com>
2025-02-26 10:28:35 -0300

    Aula sobre concatenação de variaveis

---
+++ aula02/concatenar.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hello world!<br>";
+ECHO "Hello world!<br>";
+echo "Hello world!<br>";
+EcHO "Hello world!<br>";
+
+$nome = "Everthon";
+
+$sobrenome = "Luiz";
+$numero = 4;
+
+echo $nome . " concatenação". "<br>";
+echo "${nome} <h1> ${sobrenome} </h1>";
+echo $numero . "<p> " . $nome .
+" " . $sobrenome . "</p>";
+
+?>
