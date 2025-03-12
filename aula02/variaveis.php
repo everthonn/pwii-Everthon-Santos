77b340c55ef02a58a58dd5f4e9b6d962ed5a4916
everthon luiz <everthondier4@gmail.com>
2025-03-12 10:34:40 -0300

    aula sobre variaveis

---
+++ aula02/variaveis.php
@@ -0,0 +1,17 @@
+<?php
+$texto = "isso é uma porra";
+$n1 = 2;
+$n2 = 3.14;
+$valor = null;
+
+echo $texto
+echo "<br>";
+echo gettype($texto), "<br";
+echo gettype($n1), "<br";
+echo gettype($n2), "<br";
+echo gettype($valor);
+
+
+
+
+?>
