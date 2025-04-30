6d22a9f7f3f050eeb99df77b3cf8eaeea20eda36
everthon luiz <everthondier4@gmail.com>
2025-04-30 10:37:13 -0300

    aula 6

---
+++ bimestre2/aula06/welcome.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        
+        Welcome
+        <?php echo $_GET["name"]; ?><br>
+        Your E-mail address is:
+        <?php echo $_GET ["E-mail"]; ?>
+        
+    </body>
+</html>
